4cf497b1bbcc2636c51111cb75fda605da5301e1
cindy9899 <yeonhoo9899@gmail.com>
2022-08-27 18:13:03 +0900

    lecture 4

---
+++ src/main/kotlin/practice/lec03/Lec04_1.kt
@@ -0,0 +1,15 @@
+package practice.lec03
+
+//data class Money {
+//    val amount: Long
+//}
+
+//operator fun plus(other: Money): Money {
+//    return Money(this.amount+other.amount)
+//}
+
+fun main(){
+//    val money1=Money(1_000L)
+//    val money2=Money(2_000L)
+//    println(money1+money2)
+}
---
+++ src/main/kotlin/practice/lec04/Lec04.kt
@@ -0,0 +1,15 @@
+package practice.lec04
+
+fun main() {
+//    val money1 = JavaMoney(2000)
+//    val money2 = money1
+//    val money3 = JavaMoney(2000)
+    val money1 = 1000
+    val money2= 2000
+
+    //money1===money2
+    //money1==money2
+    if(money1 > money2) {
+        println("money1이 더 큽니다.")
+    }
+}
